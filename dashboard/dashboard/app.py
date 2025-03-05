c89541512914efec582dc38312c1264985089c2f
shiva jalabagari <shivajalabagari666@gmail.com>
2025-03-05 09:10:41 +0000

    added chnages

--- dashboard/dashboard/app.py
+++ dashboard/dashboard/app.py
@@ -1,12 +1,10 @@
-# dashboard/app.py
 from flask import Flask, render_template
 from pymongo import MongoClient
-from config.db_config import MONGO_URI, MONGO_DB, MONGO_COLLECTION
 
 app = Flask(__name__)
-client = MongoClient(MONGO_URI)
-db = client[MONGO_DB]
-collection = db[MONGO_COLLECTION]
+client = MongoClient("mongodb://localhost:27017")
+db = client["analytics"]
+collection = db["interactions"]
 
 @app.route("/")
 def index():
--- setup.sh
+++
@@ -1,7 +0,0 @@
-# setup.sh
-#!/bin/bash
-pip install -r requirements.txt
-docker-compose up -d
-python data_generator.py &
-python consumer.py &
-cd dashboard && python app.py &
